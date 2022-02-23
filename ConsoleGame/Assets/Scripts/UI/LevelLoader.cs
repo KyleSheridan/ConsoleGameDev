e5796258e56aec2cbdad3b65367078f2dd748b51
Demi-ayan <56345561+Demi-ayan@users.noreply.github.com>
2022-02-23 18:19:07 +0000

    Loading bar done

--- ConsoleGame/Assets/Scripts/UI/LevelLoader.cs
+++ ConsoleGame/Assets/Scripts/UI/LevelLoader.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider progressSlider;
+    public Text progressText;
+
     public void LoadScene(int sceneIndex)
     {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
 
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            int progressInt = Mathf.RoundToInt(progress * 100f);
+
+            Debug.Log(progress * 100f + "% loaded");
+
+            progressSlider.value = progress;
+            progressText.text = progressInt + "%";
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
