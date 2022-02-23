e0c72ad16ddcb91a486b770533d7dc95b9a8082a
Demi-ayan <56345561+Demi-ayan@users.noreply.github.com>
2022-02-23 16:53:40 +0000

    Loading screen draft

---
+++ ConsoleGame/Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+
+    }
+}
