fa3d21f1069ea40e86d9157bbf11b6b4e95704cb
Demi-ayan <56345561+Demi-ayan@users.noreply.github.com>
2022-02-27 20:29:01 +0000

    UI controller set up

---
+++ ConsoleGame/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject pauseMenu, optionsMenu;
+
+    public GameObject pauseFirstButton, optionsFirstButton, optionsClosedButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Fire3"))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        if (!pauseMenu.activeInHierarchy)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+
+            //clear selected object 
+            EventSystem.current.SetSelectedGameObject(null);
+            //set a new selected object
+            EventSystem.current.SetSelectedGameObject(pauseFirstButton);
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            optionsMenu.SetActive(false);
+        }
+    }
+
+    public void OpenOptions()
+    {
+        //clear selected object 
+        EventSystem.current.SetSelectedGameObject(null);
+        //set a new selected object
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void CloseOptions()
+    {
+        //clear selected object 
+        EventSystem.current.SetSelectedGameObject(null);
+        //set a new selected object
+        EventSystem.current.SetSelectedGameObject(optionsClosedButton);
+    }
+}
