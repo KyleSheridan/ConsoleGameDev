be9698f66f27d78a225c55f5f839c18bc65bb498
Demi-ayan <56345561+Demi-ayan@users.noreply.github.com>
2022-05-05 16:14:29 +0100

    AI take damage set up

--- ConsoleGame/Assets/Scripts/AI/AIManager.cs
+++ ConsoleGame/Assets/Scripts/AI/AIManager.cs
@@ -2,12 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIManager : MonoBehaviour
 {
     public float aiHealth = 50f;
     public bool dead = false;
 
+    AgentChase agentChase;
+    AgentNav agentNav;
+    NavMeshAgent navMesh;
+
+    private void Start()
+    {
+        agentChase = GetComponent<AgentChase>();
+        agentNav = GetComponent<AgentNav>();
+        navMesh = GetComponent<NavMeshAgent>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -15,7 +26,20 @@ public class AIManager : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        //
+        if(other.gameObject.tag == "Melee")
+        {
+            aiHealth -= 0; //Value
+        }
+        
+        if(other.gameObject.tag == "Ranged")
+        {
+            aiHealth -= 0; //Value
+        }
+        
+        if(other.gameObject.tag == "Magic")
+        {
+            aiHealth -= 0; //Value
+        }
     }
 
     public void Die()
@@ -23,7 +47,10 @@ public class AIManager : MonoBehaviour
         if(aiHealth <= 0)
         {
             dead = true;
-            //StartCoroutine(DestroyWait(4f));
+            agentChase.enabled = false;
+            agentNav.enabled = false;
+            navMesh.isStopped = true;
+            StartCoroutine(DestroyWait(6f));
 
             
         }
@@ -32,7 +59,7 @@ public class AIManager : MonoBehaviour
     IEnumerator DestroyWait(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        //Destroy(gameObject);
+        Destroy(gameObject);
     }
 
 }
