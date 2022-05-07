8fd8d6a1309e4ea5e1fc6ed5faa3bbb636ab2617
Demi-ayan <56345561+Demi-ayan@users.noreply.github.com>
2022-05-07 07:43:17 +0100

    Crate and Bottle break sound implementation

---
+++ ConsoleGame/Assets/Scripts/Gameplay/PlayerLevelComplete.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLevelComplete : MonoBehaviour
+{
+    public GameObject endLevelScreen;
+
+    //IDK HOW TO WIN YET
+}
