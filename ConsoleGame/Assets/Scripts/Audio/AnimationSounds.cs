920652a788202ad02b10019d5e07ab2cbf301c6c
Kyle Sheridan <55580454+KyleSheridan@users.noreply.github.com>
2022-05-07 23:35:02 +0100

    Sounds for ranged and magic attacks

--- ConsoleGame/Assets/Scripts/Audio/AnimationSounds.cs
+++ ConsoleGame/Assets/Scripts/Audio/AnimationSounds.cs
@@ -7,6 +7,9 @@ public class AnimationSounds : MonoBehaviour
     private FMOD.Studio.EventInstance swing;
     private FMOD.Studio.EventInstance footstep;
 
+    private FMOD.Studio.EventInstance magic;
+    private FMOD.Studio.EventInstance gunshot;
+
     void PlayLightSwing()
     {
         swing = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/PlayerAttack");
@@ -28,8 +31,6 @@ public class AnimationSounds : MonoBehaviour
     void PlayFootstep()
     {
         footstep = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Footsteps");
-        //swing.setParameterByNameWithLabel("SwingType", "Heavy");
-        //swing.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         footstep.start();
         footstep.release();
     }
@@ -40,5 +41,21 @@ public class AnimationSounds : MonoBehaviour
         footstep.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         footstep.start();
         footstep.release();
+    }
+
+    void PlayMagic()
+    {
+        magic = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/MagicAttack");
+        magic.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        magic.start();
+        magic.release();
+    }
+
+    void PlayGunshot()
+    {
+        gunshot = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Gunshot");
+        gunshot.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        gunshot.start();
+        gunshot.release();
     }
 }
